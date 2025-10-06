ec9efd138371b13be4f298a7b37d9c877cdbef54
onepercman <onepercman@onepercmans-MacBook-Pro.local>
2025-10-06 10:32:48 +0700

    🚀 update address config

--- src/features/home/constants.ts
+++ src/features/home/constants.ts
@@ -1,14 +1,14 @@
 import { Address } from "viem"
-import { bsc, bscTestnet } from "viem/chains"
+import { bsc, monadTestnet } from "viem/chains"
 import { useChainId } from "wagmi"
 
 export const addresses = {
   USDC: {
-    [bscTestnet.id]: "0xdeb82ECC2474Cb37A1f740E2d9Ce28815B815A3f",
+    [monadTestnet.id]: "0xdeb82ECC2474Cb37A1f740E2d9Ce28815B815A3f",
     [bsc.id]: "0x55d398326f99059fF775485246999027B3197955",
   },
   Sale: {
-    [bscTestnet.id]: "0x389cc206939826610FCcf2E31843349F8C36D6f4",
+    [monadTestnet.id]: "0x389cc206939826610FCcf2E31843349F8C36D6f4",
     [bsc.id]: "0x5CD6E441d267b3a0D6BE0E7929416DeE02F6b235",
   },
 }
